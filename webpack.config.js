bea4509edf6d4e7ffdc7928c5f92ec114d13745c
Misha Ponizil <misha.ponizil@gmail.com>
2015-05-14 17:12:09 -0400

    Use js-cookie#2-beta to remove jquery dep

--- webpack.config.js
+++ webpack.config.js
@@ -6,9 +6,5 @@ module.exports = {
     filename: 'localstorage-events.js',
     library: 'LSEvents',
     libraryTarget: 'umd'
-  },
-  externals: [{
-    // js-cookie (formerly jquery-cookie) requires jquery but allows it to be undefined
-    'jquery': 'this $'
-  }]
+  }
 };
